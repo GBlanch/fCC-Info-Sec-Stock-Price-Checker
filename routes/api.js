4cfd4afb65a4f50440d8fd41218045d52e046a2d
Gerry Blanch <g.blandugar@gmail.com>
2024-02-09 16:54:15 -0500

    committed to github

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,123 @@
 'use strict';
+const StockModel = require('../models').Stock;
+
+import('node-fetch').then((fetch) => {
+
+}).catch((error) => {
+
+  console.error('Failed to import node-fetch:', error);
+
+});
+
+// Create a new stock record, likes being optional
+async function createStock(stock, like, ip) {
+  const newStock = new StockModel({
+    symbol: stock,
+    likes: like ? [ip] : [],
+  });
+
+  const savedNew = await newStock.save();
+  return savedNew;
+}
+
+// Find stock data by symbol using async MongoDB query
+async function findStock(stock) {
+  return await StockModel.findOne({ symbol: stock }).exec();
+}
+
+// Save stock data based on existence of stock and likes
+async function saveStock(stock, like, ip) {
+  let saved = await findStock(stock);
+
+  if (!saved) {
+    saved = await createStock(stock, like, ip);
+
+  } else if (like && !saved.likes.includes(ip)) {
+    saved.likes.push(ip);
+    saved = await saved.save();
+  }
+
+  return saved;
+}
+
+// Fetch stock data from an external API..
+async function getStock(stock) {
+  const response = await fetch(
+    `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
+  );
+//..and extract its symbol and latest price
+  const { symbol, latestPrice } = await response.json();
+  return { symbol, latestPrice };
+}
 
 module.exports = function (app) {
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
+  app.route('/api/stock-prices').get(async function (req, res) {
+    const { stock, like } = req.query;
+
+    if (Array.isArray(stock)) {
+      console.log('stocks', stock);
+      //  Fetch stock data for two different stocks
+      const { symbol, latestPrice } = await getStock(stock[0]);
+      const { symbol: symbol2, latestPrice: latestPrice2 } = await getStock(
+        stock[1]
+      );
+
+      const firststock = await saveStock(stock[0], like, req.ip);
+      const secondstock = await saveStock(stock[1], like, req.ip);
+
+      let stockData = [];
+
+      // First stock data
+      if (!symbol) {
+        stockData.push({
+          rel_likes: firststock.likes.length - secondstock.likes.length,
+        });
+      } else {
+        stockData.push({
+          stock: symbol,
+          price: latestPrice,
+          rel_likes: firststock.likes.length - secondstock.likes.length,
+        });
+      }
+
+      // Second stock data
+      if (!symbol2) {
+        stockData.push({
+          rel_likes: secondstock.likes.length - firststock.likes.length,
+        });
+      } else {
+        stockData.push({
+          stock: symbol2,
+          price: latestPrice2,
+          rel_likes: secondstock.likes.length - firststock.likes.length,
+        });
+      }
+
+      res.json({
+        stockData,
+      });
       
+      return;
+
+    }
+
+    const { symbol, latestPrice } = await getStock(stock);
+    if (!symbol) {
+      res.json({ stockData: { likes: like ? 1 : 0 } });
+      return;
+    }
+
+    const oneStockData = await saveStock(symbol, like, req.ip);
+    console.log('One Stock Data', oneStockData);
+
+    res.json({
+      stockData: {
+        stock: symbol,
+        price: latestPrice,
+        likes: oneStockData.likes.length,
+      },
     });
-    
+
+  });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,104 @@
-const chaiHttp = require('chai-http');
+// Require chai/chai-http and main server file
 const chai = require('chai');
-const assert = chai.assert;
+const { assert, expect } = require('chai');
+const chaiHttp = require('chai-http');
 const server = require('../server');
 
+// Enable chai-http plugin 
 chai.use(chaiHttp);
+   
 
-suite('Functional Tests', function() {
+suite('Functional tests:', function () {
+  suite('', function () {
 
+    // Assert the response status, stock symbol, and price existence 
+    // after making a GET request to '/api/stock-prices/'  
+    // querying 'MSFT' as stock parameter 
+    test('#1. Viewing one stock: GET request to endpoint `/api/stock-prices/`',
+     function (done) {
+      chai
+        .request(server)
+        .get('/api/stock-prices/')
+        .set('content-type', 'application/json')
+        .query({ stock: 'MSFT' })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.stockData.stock, 'MSFT');
+          assert.exists(res.body.stockData.price, 'Price for MSFT exists');
+          done();
+        });
+    });
+    
+    // Same structure as #1, just add query parameter `like`
+    // and add an assertion for it
+    test('#2. Viewing one stock and liking it: GET request to endpoint `/api/stock-prices/`"',
+     function (done) {
+        chai
+          .request(server)
+          .get('/api/stock-prices/')
+          .set('content-type', 'application/json')
+          .query({ stock: 'AMZN', like: true })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.stockData.stock, 'AMZN');
+            assert.equal(res.body.stockData.likes, 1);
+            assert.exists(res.body.stockData.price, 'Price for AMZN exists');
+            done();
+          });
+      });
+      
+    // Iterate test #2
+    test('#3. Viewing the same stock and liking it again: GET request to endpoint `/api/stock-prices/`',
+     function (done) {
+      chai
+        .request(server)
+        .get('/api/stock-prices/')
+        .set('content-type', 'application/json')
+        .query({ stock: 'AMZN', like: true })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.stockData.stock, 'AMZN');
+          assert.equal(res.body.stockData.likes, 1);
+          assert.exists(res.body.stockData.price, 'Price for AMZN exists');
+          done();
+        });
+    });
+    // Same structure as #1, in this case the query param. is a list
+    test('#4. Viewing two stocks: GET request to endpoint `/api/stock-prices/`',
+     function (done) {
+      chai
+        .request(server)
+        .get('/api/stock-prices/')
+        .set('content-type', 'application/json')
+        .query({ stock: ['GOOG', 'AAPL'] })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.stockData[0].stock, 'GOOG');
+          assert.equal(res.body.stockData[1].stock, 'AAPL');
+          assert.exists(res.body.stockData[0].price, 'Price for GOOG exists');
+          assert.exists(res.body.stockData[1].price, 'Price for AAPL exists');
+          done();
+        });
+    });
+    // Same structure as #4, plus asserting for existance of `rel-likes` property
+    test('#5. Viewing two stocks and liking them: GET request to endpoint `/api/stock-prices/`',
+     function (done) {
+        chai
+          .request(server)
+          .get('/api/stock-prices/')
+          .set('content-type', 'application/json')
+          .query({ stock: ['GOOG', 'AAPL'], like: true })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.stockData[0].stock, 'GOOG');
+            assert.equal(res.body.stockData[1].stock, 'AAPL');
+            assert.exists(res.body.stockData[0].price, 'Price for GOOG exists');
+            assert.exists(res.body.stockData[1].price, 'Price for AAPL exists');
+            assert.exists(res.body.stockData[0].rel_likes, 'rel_likes exist');
+            assert.exists(res.body.stockData[1].rel_likes, 'rel_likes exist');
+            done();
+          });
+      });
+
+  });
 });
